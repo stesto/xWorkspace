37249bc87aa85387bd406ab66ccc1304c2d1d2d1
Drust1 <rust.daniel01@gmail.com>
2022-06-16 18:40:16 +0200

    kalender

---
+++ js/demo.js
@@ -0,0 +1,8 @@
+var events = [
+  {'Date': new Date(2022, 5, 13), 'Title': 'Raum 6A 325 (12-14)'},
+  {'Date': new Date(2022, 5, 21), 'Title': 'Raum 6A 123 (14-16)'},
+  
+];
+var settings = {};
+var element = document.getElementById('caleandar');
+caleandar(element, events, settings);
