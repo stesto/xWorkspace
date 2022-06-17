46c97220d2954b0d13ad7e42d3b006b2d48e6e8f
Leleat <atule@pm.me>
2022-06-17 09:37:14 +0200

    Kalender: Falls sessionEvents null ist, kann man nicht durchlaufen

--- js/demo.js
+++ js/demo.js
@@ -1,18 +1,19 @@
 var events = [];
 
 var sessionEvents = sessionStorage.getItem('reservations');
-if (sessionEvents != null)
+if (sessionEvents !== null) {
   sessionEvents = JSON.parse(sessionEvents);
 
-sessionEvents.forEach(element => {
-  var date = new Date(element.datum); 
-  console.log(date);
-  events.push({
-    Date: new Date(date.getFullYear(), date.getMonth(), date.getDate()),
-    Title: element.platz.raum,
-    Link: '#'
+  sessionEvents.forEach(element => {
+    var date = new Date(element.datum);
+    console.log(date);
+    events.push({
+      Date: new Date(date.getFullYear(), date.getMonth(), date.getDate()),
+      Title: element.platz.raum,
+      Link: '#'
+    });
   });
-});
+}
 
 var settings = {};
 
