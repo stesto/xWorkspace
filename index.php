06b6696784e7b9a9d4c12e22c99d3ee4d40971f8
Leleat <atule@pm.me>
2022-06-30 00:24:37 +0200

    Füge Landing Page hinzu

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html class="h-100" lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Demo</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+	</head>
+	<body class="h-100" style="background-color: #150c3f">
+		<div class="container text-light text-center p-5">
+			<img class="img-fluid w-60 pb-5" src="CS_14_web.png" alt="">
+			<div class="container d-grid gap-4 pb-4">
+				<h1>xWorkspace</h1>
+				<h5>Dein Portal für Raumbuchungen</h5>
+				<p>Die Kollaborationsplattform ist mit Hingabe und Engagement für den zukünftigen Erfolg unserer MitarberiterInnen und Studierenden ein Vorreiter im Buchungsbereich. Wir bieten eine unvergleichliche Plattform, die die intellektuelle Entfaltung und die Mitarbeit der Studierenden und MitarbeiterInnen fördert.</p>
+			</div>
+			<a href="login.php" class="btn btn-primary text-dark" role="button" style="background-color:white; border:white">Log in</a>
+		</div>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+	</body>
+</html>
