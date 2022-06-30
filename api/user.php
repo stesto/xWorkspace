ab1a96303f391c137912282493b4c50513d5fa57
Oliver Ollech <bln@gmx.com>
2022-06-30 21:51:23 +0200

    user api

---
+++ api/user.php
@@ -0,0 +1,22 @@
+<?php
+    require_once "_db.php";
+
+    if (isset($_GET["Name"]) && isset($_GET["Password"])) {
+        $name = $_GET["Name"];
+        $password = $_GET["Password"];
+
+        $query = 
+        "SELECT 
+             ID, Name
+        FROM Benutzer  
+        WHERE Name = '$name' AND Password = '$password';";
+
+        $users = db::getInstance()->query_to_array($query);
+
+        echo json_encode($users);
+    }
+    else {
+        echo "[]";
+    }
+
+        
