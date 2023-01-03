b8a3eb63a3ef9af2829ecb8e5e9b95bf0b06ada0
sumeyyedz <119520304+sumeyyedz@users.noreply.github.com>
2023-01-03 13:08:56 +0100

    Update login.php

--- login.php
+++ login.php
@@ -9,141 +9,52 @@
     <html>
         <head>
         <title>xWorkspace Login</title>
-        <link rel="icon" type="image/x-icon" href="media/favicon.ico">
+        <link re l="icon" type="image/x-icon" href="media/favicon.ico">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/loading.css"/>
+        <link rel="stylesheet" href="style_login_neu.css"/>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;800&display=swap" rel="stylesheet">
 
 
-    <style>
-        * {
-            font-family: 'Poppins', sans-serif;
-        }
-        
-        [v-cloak] {
-            visibility: hidden !important;
-        }
-
-        .login-form {
-            border-radius: 15px;
-            background-color: rgba(0,0,0, 0.4);
-            display: flex;
-            justify-content: center;
-            margin-top: 80px;
-            width: 650px;
-            margin-left: auto;
-            margin-right: auto;
-            flex-direction: column;
-            padding: 40px;
-            backdrop-filter: blur(25px);
-        }
-
-        #background {
-            width: 100%;
-            height: 100%;
-            background-image: url(CS_14_web.png);
-            background-size: cover;
-            position: absolute;
-            top: 0;
-            left: 0;
-            z-index: -999999;
-            filter: blur(8px);
-            -webkit-filter: blur(8px);
-        }
-
-        #header {
-            font-family: 'Poppins', sans-serif;
-            font-weight: 800;
-            font-size: 48pt;
-            color: white;
-            text-shadow: 5px 5px rgb(0 0 0 / 30%);
-            margin: 0 0 20px 0;
-            text-align: center;
-        }
-
-        #banner {
-
-            text-align: center;
-                        }
-
-        ::placeholder {
-            font-family: 'Poppins', sans-serif;
-            color: #FFF9;
-            font-weight: 500;
-        }
-
-        input[type=text], input[type=password] {
-            padding: 5px;
-            font-size: 16pt;
-            color: white;
-            font-weight: 500;
-            background-color: #0000;
-            width: 100%;
-            margin: 25px 0 0 0;
-            display: inline-block;
-            border: none;
-            border-bottom: 2px solid #ccc;
-            box-sizing: border-box;
-        }
-
-        #login-btn {
-            background-color: rgb(255 255 255 / 20%);
-            font-weight: 500;
-            color: white;
-            padding: 14px 20px;
-            margin: 8px 0;
-            border: none;
-            cursor: pointer;
-            width: 30%;
-            float: right;
-            font-size: 12pt;
-            margin: 0;
-            border-radius: 3px;
-        }
-
-        button:hover {
-            opacity: 0.8;
-            background-color: #2c3e50;
-        }
-
-        .imgcontainer {
-            text-align: center;
-            margin: 24px 0 12px 0;
-        }
-
-        img.avatar {
-            width: 40%;
-            border-radius: 50%;
-        }
-
-        .hidden {
-            visibility: hidden;
-        }
-    </style>
-
+    
 </head>
 
     <body>
-        <div class="login-form" id="vue-body">
+        
+        <div class="body" id="vue-body">
 
                 <h1 id="banner"> <img src="media/xWorkspace Banner.svg" alt="" width="500"></h1>
-                <h6 id="header">Herzlich Willkommen!</h6>
+          
 				<div class="container">
-					<!-- <label for="username"><b>Benutzername</b></label> -->
-					<input type="text" placeholder="Name" name="username" v-model="username" v-on:keydown.enter="login">
-
-					<!-- <label for="psw"><b>Passwort</b></label> -->
-					<input type="password" placeholder="Passwort" name="psw" v-model="password" v-on:keydown.enter="login">
-                    <div style="display: flex; justify-content: space-between; margin-top: 40px;">
-                        <div v-cloak :class="{ hidden: !loading }" class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
-                        <button id="login-btn" v-on:click="login" :disabled="nameMissing || passwordMissing || loading">Anmelden</button>
+                <div class="title">Herzlich Willkommen!</div>
+                 <div class="content">
+                 <div class="user-details">
+                 <div class="input-box">
+                 <span class="details">Name</span>
+                 <input type="text" placeholder="Gib deinen Namen ein" name="username"v-model="username" v-on:keydown.enter="login">
+                 </div>
+                 <div class="input-box">
+                 <span class="details">Passwort</span>
+                 <input type="text" placeholder="Gib dein Passwort ein" name="psw" v-model="password" v-on:keydown.enter="login">
+                 </div>
+                <div style="display: flex; justify-content: space-between; margin-top: 40px;">
+                        <div v-cloak :class="{ hidden: !loading }" class="lds-ellipsis"><div></div><div></div><div></div>
+                        <div class="right">
+                        <button  id="login-btn" v-on:click="login" :disabled="nameMissing || passwordMissing || loading">Login</button>
+                        </div>
+                        <div class="left">
+                        <a href="form.php">    
+                        <button id= "register-btn">Registrieren</button>
+                        </a>
+                        <div>
+                        </div>
+                        <div>
+                        </div>   
                     </div>
-				</div>								
-
-				</div>
-        </div>
+                    </div>
+                    </div>
+    
 
         <div id="background"></div>
 
@@ -153,3 +64,4 @@
         <script src="js/login.js"></script>
     </body>
 </html>
+        
